3b67fc11c5f6307a0b37ef8e39e8f760addb4f29
Aleksandra0 <66222827+Aleksandra0@users.noreply.github.com>
2021-04-20 09:30:56 +0200

    Update form.php

--- Salon_fryzjerski/Subpages/form.php
+++ Salon_fryzjerski/Subpages/form.php
@@ -8,51 +8,6 @@
         <title>Rejestracja na wizytę</title>
         <script src="../Scripts/form_validation.js"></script> 
     </head>
-    <body>
-        <div class="back">
-            <button class="button"> <a href="../index.html"> Powrót do strony głównej </a></button>
-        </div>
-        <div class="subtitle">
-            <h2> Zarejestruj się </h2>
-        </div>
-        <div id="form_area">
-            <form method="get" action="<?php print($_SERVER['PHP_SELF']);?>">
-                <div class="inputs">
-                <div class="inputs_column">
-                    <label> Imię: <input type="text" name="firstname" id="firstname"  value="<?php echo isset($_GET["firstname"]) ? $_GET["firstname"] : ''; ?>" required/> </label>
-                    <label> Nazwisko: <input type="text" name="surname" id="surname" value="<?php echo isset($_GET["surname"]) ? $_GET["surname"] : ''; ?>"  required/> </label>
-                    <label> Email: <input type="email" name="email" id="email" value="<?php echo isset($_GET["email"]) ? $_GET["email"] : ''; ?>" required/> </label>
-                    <label> Telefon kontaktowy: <input placeholder="bez +48" type="tel" name="phone" id="phone" value="<?php echo isset($_GET["phone"]) ? $_GET["phone"] : ''; ?>" required/> </label>
-                </div>
-                <div class="inputs_column">
-                    <label> Data wizyty: <input type="date" name="date" id="date" value="<?php echo isset($_GET["date"]) ? $_GET["date"] : ''; ?>" required/> </label>
-                    <label> Godzina wizyty:
-                    <select name="hour" id="hour">
-                        <option>08:00</option>
-		                <option>10:00</option>
-		                <option>12:00</option>
-                        <option>14:00</option>
-                        <option>16:00</option>
-	                </select>
-                    </label>
-                    <label> Typ wizyty:
-                    <select name="type" id="type">
-                        <option> Strzyżenie </option>
-		                <option> Koloryzacja </option>
-		                <option> Strzyżenie i koloryzacja</option>
-                        <option> Modelowanie </option>
-                        <option> Inna </option>
-	                </select>
-                    </label>
-                </div>
-                </div>
-                <p>Uwaga: możesz zarejestrować się najwcześniej na jutro </p>
-                <input type="submit" name="submit" value="Zarejestruj się" class="button_reverse" onclick="validation()"/>
-                <input type="hidden" value="0" id="val" name="val">
-            </form>
-        </div>
-        <div id="result"></div>
-    </body>
     <?php 
         $servername = "localhost";
         $username = "admin";
@@ -99,7 +54,69 @@
                 print("Błąd tworzenia tabeli: ".$connection->error);
             }
         }  
-
+        $query_taken = "SELECT date_of_visit, hour FROM wizyty";
+        $result_taken = $connection->query($query_taken);
+        $message="";
+        if ($result_taken->num_rows > 0)
+        {
+        while($row = $result_taken->fetch_assoc())
+        {
+            $message = $message.$row['date_of_visit'].": ".$row['hour']."\n";
+        }
+        }
+        else{
+            $message="Brak zajętych terminów";
+        }
+    ?>
+    <body>
+        <div class="back">
+            <button class="button"> <a href="../index.html"> Powrót do strony głównej </a></button>
+        </div>
+        <div class="subtitle">
+            <h2> Zarejestruj się </h2>
+        </div>
+        <div id="form_area">
+            <form method="get" action="<?php print($_SERVER['PHP_SELF']);?>">
+                <div class="inputs">
+                <div class="inputs_column">
+                    <label> Imię: <input type="text" name="firstname" id="firstname"  value="<?php echo isset($_GET["firstname"]) ? $_GET["firstname"] : ''; ?>" required/> </label>
+                    <label> Nazwisko: <input type="text" name="surname" id="surname" value="<?php echo isset($_GET["surname"]) ? $_GET["surname"] : ''; ?>"  required/> </label>
+                    <label> Email: <input type="email" name="email" id="email" value="<?php echo isset($_GET["email"]) ? $_GET["email"] : ''; ?>" required/> </label>
+                    <label> Telefon kontaktowy: <input placeholder="bez +48" type="tel" name="phone" id="phone" value="<?php echo isset($_GET["phone"]) ? $_GET["phone"] : ''; ?>" required/> </label>
+                </div>
+                <div class="inputs_column">
+                    <label> Data wizyty: <input type="date" name="date" id="date" value="<?php echo isset($_GET["date"]) ? $_GET["date"] : ''; ?>" required/> </label>
+                    <label> Godzina wizyty:
+                    <select name="hour" id="hour">
+                        <option>08:00</option>
+		                <option>10:00</option>
+		                <option>12:00</option>
+                        <option>14:00</option>
+                        <option>16:00</option>
+	                </select>
+                    </label>
+                    <label> Typ wizyty:
+                    <select name="type" id="type">
+                        <option> Strzyżenie </option>
+		                <option> Koloryzacja </option>
+		                <option> Strzyżenie i koloryzacja</option>
+                        <option> Modelowanie </option>
+                        <option> Inna </option>
+	                </select>
+                    </label>
+                    <label> Zajęte terminy:
+                    <textarea name="taken" readonly> <?=$message?> </textarea>
+                    </label>
+                </div>
+                </div>
+                <p>Uwaga: możesz zarejestrować się najwcześniej na jutro </p>
+                <input type="submit" name="submit" value="Zarejestruj się" class="button_reverse" onclick="validation()"/>
+                <input type="hidden" value="0" id="val" name="val">
+            </form>
+        </div>
+        <div id="result"></div>
+    </body>
+    <?php 
        if(isset($_GET['submit']))
         {
             $czy = $_GET['val'];
